17b8305642c5761383e2ce40981c7af2824047d4
Aryan Bhimani <aryan.bhimani93@gmail.com>
2024-12-08 22:32:01 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mindsphere/Home%20Screen/Home.dart';
 import 'package:mindsphere/Onboarding%20Screen/Onboarding_Screen.dart';
-import 'package:mindsphere/Sign%20in%20Screen/login.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform, // Use currentPlatform directly
+    options: DefaultFirebaseOptions.currentPlatform, 
   );
   runApp(const MyApp());
 }
